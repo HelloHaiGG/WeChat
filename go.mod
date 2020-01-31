20a5e224a50d2d529f7f2562a2a89d5b79a9e600
邢庆海 <1296985852@qq.com>
2020-01-31 17:23:43 +0800

    redis config

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/HelloHaiGG/WeChat
+
+go 1.12
+
+require (
+	github.com/go-redis/redis v6.15.6+incompatible
+	github.com/onsi/ginkgo v1.11.0 // indirect
+	github.com/onsi/gomega v1.8.1 // indirect
+	gopkg.in/yaml.v2 v2.2.8
+)
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/HelloHaiGG/WeChat/common/iredis"
+	"github.com/HelloHaiGG/WeChat/config"
+)
+
+func main() {
+	config.Init()
+	fmt.Println(config.APPCfg.Redis)
+	iredis.Init(&iredis.IOptions{
+		Host:     config.APPCfg.Redis.Host,
+		Port:     config.APPCfg.Redis.Port,
+		DB:       config.APPCfg.Redis.DB,
+		Password: config.APPCfg.Redis.Password,
+	})
+}
