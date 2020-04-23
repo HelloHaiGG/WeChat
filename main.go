ff24ca4df22490f3fb78cc47904bdfb993057b47
mac126 <mac126@mac126deMacBook-Pro.local>
2020-04-23 11:12:57 +0000

    review

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/HelloHaiGG/WeChat/common"
 	"github.com/HelloHaiGG/WeChat/common/igorm"
+	"github.com/HelloHaiGG/WeChat/common/imongo"
+	"github.com/HelloHaiGG/WeChat/common/iredis"
 	"github.com/HelloHaiGG/WeChat/config"
 	"github.com/HelloHaiGG/WeChat/listener"
 	"github.com/HelloHaiGG/WeChat/router"
@@ -14,37 +16,37 @@ import (
 
 func main() {
 	config.Init()
-	//iredis.Init(&iredis.IOptions{
-	//	Host:     config.APPCfg.Redis.Host,
-	//	Port:     config.APPCfg.Redis.Port,
-	//	DB:       config.APPCfg.Redis.DB,
-	//	Password: config.APPCfg.Redis.Password,
-	//})
-	//imongo.Init(&imongo.IOptions{
-	//	Host:       config.APPCfg.Mongo.Host,
-	//	Port:       config.APPCfg.Mongo.Port,
-	//	DB:         config.APPCfg.Mongo.DB,
-	//	User:       config.APPCfg.Mongo.User,
-	//	Password:   config.APPCfg.Mongo.Password,
-	//	AuthSource: config.APPCfg.Mongo.AuthSource,
-	//	TimeOut:    config.APPCfg.Mongo.Timeout,
-	//})
+	iredis.Init(&iredis.IOptions{
+		Host:     config.APPCfg.Redis.Host,
+		Port:     config.APPCfg.Redis.Port,
+		DB:       config.APPCfg.Redis.DB,
+		Password: config.APPCfg.Redis.Password,
+	})
+	imongo.Init(&imongo.IOptions{
+		Host:       config.APPCfg.Mongo.Host,
+		Port:       config.APPCfg.Mongo.Port,
+		DB:         config.APPCfg.Mongo.DB,
+		User:       config.APPCfg.Mongo.User,
+		Password:   config.APPCfg.Mongo.Password,
+		AuthSource: config.APPCfg.Mongo.AuthSource,
+		TimeOut:    config.APPCfg.Mongo.Timeout,
+	})
 
 	igorm.Init("chat")
 
 	//聊天记录备份开启
 	go listener.RecordChanListener()
-	//启动路由
+	////启动路由
 	//if err := testrouter.Router().Run(iris.Addr(":2428")); err != nil {
 	//	log.Fatalf("Router Run Err:%v", err)
 	//}
 
-	//启动路由 二
-	//listener, err := net.Listen("tcp", ":2428")
+	////启动路由 二
+	//listen, err := net.Listen("tcp", ":2428")
 	//if err != nil {
 	//	log.Fatalf("Router Run Err:%v", err)
 	//}
-	//err = testrouter.Router().Run(iris.Listener(listener))
+	//err = testrouter.Router().Run(iris.Listener(listen))
 	//if err != nil {
 	//	log.Fatalf("Router Run Err:%v", err)
 	//}
